a976f4d023c932445652cc0d537fd297adc429d2
MISAGITHAY <125407240+Rabelaqbv@users.noreply.github.com>
2023-05-09 16:16:24 -0300

    Add files via upload

---
+++ seravogal.py
@@ -0,0 +1,14 @@
+#Faça um programa que peça para o usuário digitar um letra
+#E verifique se a letra é vogal ou consoante
+
+letrinha=input("Digite uma letra:\n")
+if letrinha=="a":
+    print("Você digitou uma vogal")
+elif letrinha=="e":
+    print("Você digitou uma vogal")
+elif letrinha=="i":
+    print("Você digitou uma vogal")
+else:
+    print("Você digitou uma consoante")
+if letrinha=="j":
+    print("Você digitou a inicial do amor da sua vida.")
